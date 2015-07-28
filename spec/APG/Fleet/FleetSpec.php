8dd91810d98a55aa97f4fc4733f559ecd29a69c7
Matt <me@mattbutterworth.co.uk>
2015-07-28 20:18:00 +0100

    Updated test

--- spec/APG/Fleet/FleetSpec.php
+++ spec/APG/Fleet/FleetSpec.php
@@ -10,13 +10,18 @@ class FleetSpec extends ObjectBehavior
     function it_should_throw_exception_when_given_invalid_path()
     {
         $this->beConstructedWith('AFA', 'test');
-        $this->shouldThrow('Exception')->during('__construct', ['AFA', 'thisfolderdoesnotexist']);
+        $this->shouldThrow('Exception')->duringInstantiation();
     }
 
     function it_is_initializable()
     {
         $this->beConstructedWith('AFA', 'test');
         $this->shouldHaveType('APG\Fleet\Fleet');
+    }
+
+    function it_should_return_all_aircraft()
+    {
+
     }
 
 
