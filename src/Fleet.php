f388f37a263027c04cd09689152e8cf2944fb635
Matt <me@mattbutterworth.co.uk>
2015-07-31 00:33:30 +0100

    Added caching mechanism.  When .cache file does not exist,  saves serialized version of AIrcraftCollection,  to prevent expensive scanning of directories.

--- src/Fleet.php
+++ src/Fleet.php
@@ -8,6 +8,7 @@ class Fleet
 {
     protected $airline;
     protected $basePath;
+    protected $cacheLocation = 'cache/aircraft.cache';
 
     /**
      * @param $airline
@@ -15,7 +16,6 @@ class Fleet
      * @param \Pimple\Container $ioc
      * @throws \Exception
      */
-    public function __construct($airline, $path)
     public function __construct($airline, $path, \Pimple\Container $ioc)
     {
         if (!is_dir($path)) {
@@ -23,22 +23,57 @@ class Fleet
         }
 
         $this->airline = $airline;
-        $this->basePath = $path;
         $this->ioc = $ioc;
         $this->ioc['basePath'] = $path;
     }
 
     public function getAllAircraft()
     {
+        if ($this->isCached()) {
+            $aircraftCollection = $this->readCache();
+            return $aircraftCollection;
+        }
+
         $fileObjects = $this->ioc['RecursiveIterator'];
         $aircraftCollection = $this->ioc['AircraftCollection'];
 
-        foreach ($objects as $object) {
-            if ($object->getFileName === 'aircraft.json') {
-                $json = file_get_contents($object->getPathName);
-                
+        foreach ($fileObjects as $file) {
+            if ($file->getFileName() === 'aircraft.json') {
+                $json = file_get_contents($file->getPathName());
+                if ($json) {
+                    $aircraftConfig = json_decode($json);
+                    $aircraftCollection->push($aircraftConfig);
+                }
             }
         }
+        $this->writeCache($aircraftCollection->all());
+
+        return $aircraftCollection->all();
+    }
+
+    /**
+     * @return bool
+     */
+    protected function isCached()
+    {
+        return file_exists($this->cacheLocation);
+    }
+
+    /**
+     * @return mixed
+     */
+    protected function readCache()
+    {
+        return unserialize(file_get_contents($this->cacheLocation));
+    }
+
+    /**
+     * @param AircraftCollection $aircraftCollection
+     * @return void
+     */
+    protected function writeCache(array $aircraftCollection)
+    {
+        file_put_contents($this->cacheLocation, serialize($aircraftCollection));
     }
 
 }
