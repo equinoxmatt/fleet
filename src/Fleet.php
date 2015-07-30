09f97b73aec566e85ceeccfaedd4f32065d5e400
Matt <me@mattbutterworth.co.uk>
2015-07-31 00:29:19 +0100

    Changed code to use IOC container.  Added example usage file.

--- src/Fleet.php
+++ src/Fleet.php
@@ -13,9 +13,11 @@ class Fleet
     /**
      * @param $airline
      * @param $path
+     * @param \Pimple\Container $ioc
      * @throws \Exception
      */
     public function __construct($airline, $path)
+    public function __construct($airline, $path, \Pimple\Container $ioc)
     {
         if (!is_dir($path)) {
             throw new \Exception('Directory does not exist.');
@@ -23,11 +25,15 @@ class Fleet
 
         $this->airline = $airline;
         $this->basePath = $path;
+        $this->ioc = $ioc;
+        $this->ioc['basePath'] = $path;
     }
 
     public function getAllAircraft()
     {
         $objects = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($this->basePath));
+        $fileObjects = $this->ioc['RecursiveIterator'];
+        $aircraftCollection = $this->ioc['AircraftCollection'];
 
         foreach ($objects as $object) {
             if ($object->getFileName === 'aircraft.json') {
